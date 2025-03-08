8bcc175d4b309d79718ef50aa707fd070edfdb04
viromancer <jacob.e.ford@gmail.com>
2025-03-07 21:53:35 -0600

    Closing the attribute menu

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Copyright WiseBit Studios
+
+
+#include "AuraGameplayTags.h"
+
+AuraGameplayTags::AuraGameplayTags()
+{
+}
+
+AuraGameplayTags::~AuraGameplayTags()
+{
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,15 @@
+// Copyright WiseBit Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class AURA_API AuraGameplayTags
+{
+public:
+	AuraGameplayTags();
+	~AuraGameplayTags();
+};
