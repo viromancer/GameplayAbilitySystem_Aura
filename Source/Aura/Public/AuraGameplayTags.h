c1198eb3c931b4edb7f6a05dead32ca3af4e4e9a
viromancer <jacob.e.ford@gmail.com>
2025-03-09 19:57:12 -0500

    Native Gameplay Tags

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -3,13 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 
 /**
- * 
+ * AuraGameplayTags
+ *
+ * Singleton containing native Gameplay Tags
  */
-class AURA_API AuraGameplayTags
+
+struct FAuraGameplayTags
 {
 public:
-	AuraGameplayTags();
-	~AuraGameplayTags();
+    static const FAuraGameplayTags& Get() { return GameplayTags; }
+    static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Primary_Strength;
+	FGameplayTag Attributes_Primary_Intelligence;
+	FGameplayTag Attributes_Primary_Resilience;
+	FGameplayTag Attributes_Primary_Vigor;
+
+	FGameplayTag Attributes_Secondary_MaxHealth;
+	FGameplayTag Attributes_Secondary_MaxMana;
+	
+    FGameplayTag Attributes_Secondary_Armor;
+	FGameplayTag Attributes_Secondary_ArmorPenetration;
+	FGameplayTag Attributes_Secondary_BlockChance;
+	FGameplayTag Attributes_Secondary_CriticalHitChance;
+	FGameplayTag Attributes_Secondary_CriticalHitDamage;
+	FGameplayTag Attributes_Secondary_CriticalHitResistance;
+	FGameplayTag Attributes_Secondary_HealthRegeneration;
+	FGameplayTag Attributes_Secondary_ManaRegeneration;
+protected:
+ 
+private:
+    static FAuraGameplayTags GameplayTags;
 };
